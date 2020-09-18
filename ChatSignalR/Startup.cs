e55f22277fdeb44c8d10cdb104e5a7788a512080
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-09-18 15:33:04 +0200

    first commit

---
+++ ChatSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatSignalR.Startup))]
+namespace ChatSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
